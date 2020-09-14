315cb46d0f9435746c10c34c6cf5a37966add232
franzcwb <studioarcadacwb@gmail.com>
2020-09-13 22:20:11 -0300

    finish dinamic web site

---
+++ pages/outro-menu.php
@@ -0,0 +1,48 @@
+<section class="extras">
+<div class="center">
+    <div id="depoimentos" class="w50 left depoimentos-container">
+        <h2 class="title">Depoimentos dos nossos clientes</h2>
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem ipsum dolor sit amet consectetur adipisicing elit. recusandae
+            repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt Eius rerum
+            aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae 
+            voluptas repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit
+            Eius rerum aperiam ut"</p>
+            <p class="nome-autor">Lorem Ipsum</p>
+        </div><!--depoimento-single-->
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem ipsum dolor sit amet consectetur adipisicing elit. recusandae
+            repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt Eius rerum
+            aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae 
+            voluptas repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit
+            Eius rerum aperiam ut"</p>
+            <p class="nome-autor">Lorem Ipsum</p>
+        </div><!--depoimento-single-->
+        <div class="depoimento-single">
+            <p class="depoimento-descricao">"Lorem ipsum dolor sit amet consectetur adipisicing elit. recusandae
+            repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt Eius rerum
+            aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae 
+            voluptas repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit
+            Eius rerum aperiam ut"</p>
+            <p class="nome-autor">Lorem Ipsum</p>
+        </div><!--depoimento-single-->
+    </div><!--w50-->
+    <div id="servicos" class="w50 left servicos-container">
+        <h2 class="title">Serviços</h2>
+        <div class="servicos">
+            <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae
+                repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit? Eius rerum
+                aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae
+                repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit? Eius rerum
+                aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime recusandae
+                repellendus molestias, ipsam beatae iusto dolorem fugit placeat delectus sed deserunt ab sit? Eius rerum
+                aperiam ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+            </ul>
+        </div><!--servicos-->
+    </div><!--w50-->
+    <div class="clear"></div>
+</div><!--center-->
+</section><!--extras-->
